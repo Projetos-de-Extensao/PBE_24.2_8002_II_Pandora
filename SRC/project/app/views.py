97a1b1baa131a542c51ff91d9c926b3270fae414
Eduardo Parga <160931043+EduardoParga@users.noreply.github.com>
2024-11-01 16:24:12 -0300

    teste

---
+++ SRC/project/app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .models import Employee, Department
+from .serializers import EmployeeSerializer, DepartmentSerializer
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+

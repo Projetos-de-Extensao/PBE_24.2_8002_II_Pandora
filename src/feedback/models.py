2b0b79a2cc72a61364f0f2de9b0af18e152a4690
EduardoParga <edu.f.parga@gmail.com>
2024-11-19 02:08:06 -0300

    rest

---
+++ src/feedback/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(default="default@example.com")
+    role = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
---
+++ src/feedback/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import EmployeeViewSet
+
+router = routers.DefaultRouter()
+router.register(r'employees', EmployeeViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
